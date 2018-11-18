015e9108fc282014b74661f9fd8c2a993f40746c
Vignesh Rao <vigneshrao206@gmail.com>
2018-11-18 15:56:19 +0530

    changed to 10

--- com/numbers/MultiplicationTables.java
+++ com/numbers/MultiplicationTables.java
@@ -3,7 +3,7 @@ package com.numbers;
 public class MultiplicationTables {
 
 	public static void main(String[] args) {
-		int num=9;
+		int num=10;
 		for(int multiplier=1;multiplier<=10;multiplier++) {
 			System.out.println(num +" X "+ multiplier+" = "+ (num*multiplier));
 		}
