ec14dc3ebaac27cfbe203e94efbe5ab8dba2730f
Vignesh Rao <vigneshrao206@gmail.com>
2018-11-18 15:28:01 +0530

    first commit

---
+++ com/numbers/MultiplicationTables.java
@@ -0,0 +1,13 @@
+package com.numbers;
+
+public class MultiplicationTables {
+
+	public static void main(String[] args) {
+		int num=9;
+		for(int multiplier=1;multiplier<=10;multiplier++) {
+			System.out.println(num +" X "+ multiplier+" = "+ (num*multiplier));
+		}
+
+	}
+
+}
